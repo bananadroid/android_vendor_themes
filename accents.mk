cb6236d6a862c399d9b958d5fd05f02fa71c580b
spezi77 <spezi7713@gmx.net>
2019-03-23 14:58:09 +0100

    accent: Add a bunch brand accents for ThemePicker
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>
    Change-Id: Iaac19eac6e689f30c0cd34df0b14caef96680e54

--- accents.mk
+++ accents.mk
@@ -33,3 +33,26 @@ PRODUCT_PACKAGES += \
     Red \
     Teal \
     Yellow
+
+# Brand Accents
+PRODUCT_PACKAGES += \
+    AospaGreen \
+    AndroidOneGreen \
+    CocaColaRed \
+    DiscordPurple \
+    FacebookBlue \
+    InstagramCerise \
+    JollibeeCrimson \
+    MonsterEnergyGreen \
+    NextbitMint \
+    OneplusRed \
+    PepsiBlue \
+    PocophoneYellow \
+    RazerGreen \
+    SamsungBlue \
+    SpotifyGreen \
+    StarbucksGreen \
+    TwitchPurple \
+    TwitterBlue \
+    XboxGreen \
+    XiaomiOrange
