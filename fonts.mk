d667fa22aefc409296e79860849c79414ce976e0
Alex <du.alexcruz@gmail.com>
2020-01-17 22:00:55 -0500

    Fonts: Import fonts.xml from fwb
    Commit history can be found here
    https://github.com/DirtyUnicorns/android_frameworks_base/commits/q10x/data/fonts/fonts.xml
    
    Change-Id: Iae43a32ae2d3aa480af07508fd5e71801269a336

---
+++ fonts.mk
@@ -0,0 +1,5 @@
+LOCAL_PATH := vendor/themes/fonts
+
+# Fonts
+PRODUCT_PACKAGES := \
+    fonts.xml
--- themes.mk
+++ themes.mk
@@ -33,3 +33,6 @@ DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common
 
 # Allow overlays to be excluded from enforcing RRO
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay/common
+
+# Fonts
+include vendor/themes/fonts.mk
