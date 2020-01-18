f50b1a1d6e8f7a40dfaa605619985cb35860e93e
Pranav Vashi <neobuddy89@gmail.com>
2020-01-18 05:56:05 -0500

    Fonts: Add OnePlus Slate and Samsung One fonts
    Change-Id: Iad1ec93aaa83d4b1e8ed792502b08e0c4853fe70

--- fonts.mk
+++ fonts.mk
@@ -7,7 +7,9 @@ PRODUCT_PACKAGES += \
     FontLGSmartGothicSourceOverlay \
     FontRosemarySourceOverlay \
     FontSonySketchSourceOverlay \
-    FontGoogleSansSourceOverlay
+    FontGoogleSansSourceOverlay \
+    FontOneplusSlateSource \
+    FontSamsungOneSourceOverlay
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/Cagliostro-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Cagliostro-Regular.ttf \
@@ -19,4 +21,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Italic.ttf \
     $(LOCAL_PATH)/prebuilt/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Medium.ttf \
     $(LOCAL_PATH)/prebuilt/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-MediumItalic.ttf \
-    $(LOCAL_PATH)/prebuilt/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Regular.ttf
+    $(LOCAL_PATH)/prebuilt/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Regular.ttf \
+    $(LOCAL_PATH)/prebuilt/SamsungOne.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SamsungOne.ttf \
+    $(LOCAL_PATH)/prebuilt/SlateFromOP-Light.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SlateFromOP-Light.ttf \
+    $(LOCAL_PATH)/prebuilt/SlateFromOP-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SlateFromOP-Regular.ttf
