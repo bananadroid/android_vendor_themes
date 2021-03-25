057c327dcb4a568f5d9c11c16a2c3128b2966f08
Sonal Singh <sonal.singh.19993@gmail.com>
2021-03-25 07:29:37 +0000

    extras: Add LockClock Widget fonts
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Change-Id: I52f2a9e4b7e93eb4cdc5962457e39c5ade9bae14

--- fonts.mk
+++ fonts.mk
@@ -40,13 +40,16 @@ PRODUCT_PACKAGES += \
     Barlow-Medium.ttf
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/Burnstown-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Burnstown-Regular.ttf \
     $(LOCAL_PATH)/prebuilt/Cagliostro-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Cagliostro-Regular.ttf \
+    $(LOCAL_PATH)/prebuilt/Dumbledor-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Dumbledor-Regular.ttf \
     $(LOCAL_PATH)/prebuilt/FiraSans-Bold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-Bold.ttf \
     $(LOCAL_PATH)/prebuilt/FiraSans-BoldItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-BoldItalic.ttf \
     $(LOCAL_PATH)/prebuilt/FiraSans-Italic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-Italic.ttf \
     $(LOCAL_PATH)/prebuilt/FiraSans-Medium.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-Medium.ttf \
     $(LOCAL_PATH)/prebuilt/FiraSans-MediumItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-MediumItalic.ttf \
     $(LOCAL_PATH)/prebuilt/FiraSans-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-Regular.ttf \
+    $(LOCAL_PATH)/prebuilt/Gobold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Gobold.ttf \
     $(LOCAL_PATH)/prebuilt/GoogleSans-Black.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Black.ttf \
     $(LOCAL_PATH)/prebuilt/GoogleSans-BlackItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-BlackItalic.ttf \
     $(LOCAL_PATH)/prebuilt/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Bold.ttf \
@@ -68,11 +71,18 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/GoogleSansCondensed-MediumItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSansCondensed-MediumItalic.ttf \
     $(LOCAL_PATH)/prebuilt/GoogleSansCondensed-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSansCondensed-Regular.ttf \
     $(LOCAL_PATH)/prebuilt/LGSmartGothic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/LGSmartGothic.ttf \
+    $(LOCAL_PATH)/prebuilt/Mexcellent-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Mexcellent-Regular.ttf \
+    $(LOCAL_PATH)/prebuilt/Neoneon-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Neoneon-Regular.ttf \
+    $(LOCAL_PATH)/prebuilt/PhantomBold-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/PhantomBold-Regular.ttf \
+    $(LOCAL_PATH)/prebuilt/RoadRage-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/RoadRage-Regular.ttf \
     $(LOCAL_PATH)/prebuilt/Rosemary-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Rosemary-Regular.ttf \
     $(LOCAL_PATH)/prebuilt/SamsungOne.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SamsungOne.ttf \
+    $(LOCAL_PATH)/prebuilt/Shamshung.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Shamshung.ttf \
     $(LOCAL_PATH)/prebuilt/SlateFromOP-Light.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SlateFromOP-Light.ttf \
     $(LOCAL_PATH)/prebuilt/SlateFromOP-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SlateFromOP-Regular.ttf \
+    $(LOCAL_PATH)/prebuilt/Snowstorm-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Snowstorm-Regular.ttf \
     $(LOCAL_PATH)/prebuilt/SonySketch.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SonySketch.ttf \
+    $(LOCAL_PATH)/prebuilt/ThemeableFont-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/ThemeableFont-Regular.ttf \
     $(LOCAL_PATH)/prebuilt/TitilliumWeb-Bold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/TitilliumWeb-Bold.ttf \
     $(LOCAL_PATH)/prebuilt/TitilliumWeb-BoldItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/TitilliumWeb-BoldItalic.ttf \
     $(LOCAL_PATH)/prebuilt/TitilliumWeb-Italic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/TitilliumWeb-Italic.ttf \
