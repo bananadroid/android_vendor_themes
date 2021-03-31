7ef9d27fe2c12040b632818462bdeece26a4bddc
Rizky Benggolo <travarilo@gmail.com>
2021-03-31 16:36:10 +0000

    themes: Call overlay from vendor/banana
    Change-Id: I0c9e60603de3ff2968b21480d36e491b3d16e068

--- themes.mk
+++ themes.mk
@@ -1,12 +1,3 @@
-# Add theme specific overlays
-DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common
-
-# Allow overlays to be excluded from enforcing RRO
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay
-
-# Include Lineage LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/themes/overlay/dictionaries
-
 # Accents
 include vendor/themes/accents.mk
 
