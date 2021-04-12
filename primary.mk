a75380abbad59906ee2fa90d5bc5b6b8253ecdc7
Josh Fox <joshfox87@gmail.com>
2021-04-12 13:16:07 +0000

    themes: Add moar primary themes

--- primary.mk
+++ primary.mk
@@ -2,6 +2,12 @@ PRODUCT_PACKAGES += \
     PitchBlackSystemOverlay \
     PitchBlackSystemUIOverlay \
     SolarizedDarkSystemOverlay \
-    SolarizedDarkSystemUIOverlay
+    SolarizedDarkSystemUIOverlay \
+    PrimaryCharcoalBlackOverlay \
+    PrimaryClearOverlay \
+    PrimaryEtherealOverlay \
+    PrimaryFlameOverlay \
+    PrimaryNatureOverlay \
+    PrimaryOceanOverlay
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/primary
