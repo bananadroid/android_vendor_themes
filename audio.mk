ea2cf29e728bbac1868186bc1a425820af64403d
Danny Lin <danny@kdrag0n.dev>
2021-05-05 16:26:49 -0700

    audio: Use AOSP Material sounds and Material Product Sounds by default
    The default AOSP ringtone, alarm, and notification sounds are quite
    lackluster in comparison to the proprietary Pixel sounds. Replace them
    with better ones that are licensed permissively:
      - AOSP Material sounds from Android 9
        frameworks/base/data/sounds/AudioPackage14.mk
      - Material Product Sounds [1], licensed under CC-BY 4.0
        https://material.io/design/sound/sound-resources.html
    
    Change-Id: I369367db0aa48ac22a6eb68001d9bf15249d38d3

--- audio.mk
+++ audio.mk
@@ -4,12 +4,6 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/notification
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ringtones,$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ui,$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui)
 
-# Change default sounds
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone=The_big_adventure.ogg \
-    ro.config.notification_sound=Popcorn.ogg \
-    ro.config.alarm_alert=Bright_morning.ogg
-
 # Include Google Sounds for all
 PRODUCT_PACKAGES += \
     SoundPickerPrebuilt
