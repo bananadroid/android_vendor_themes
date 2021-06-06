0c0eec62d6c7dc7269e4b3c3e737d70dd0a03375
Rizky Benggolo <travarilo@gmail.com>
2021-06-06 17:08:36 +0000

    Revert "themes: Add moar primary themes"
    This reverts commit a75380abbad59906ee2fa90d5bc5b6b8253ecdc7.

--- primary.mk
+++ primary.mk
@@ -2,12 +2,6 @@ PRODUCT_PACKAGES += \
     PitchBlackSystemOverlay \
     PitchBlackSystemUIOverlay \
     SolarizedDarkSystemOverlay \
-    SolarizedDarkSystemUIOverlay \
-    PrimaryCharcoalBlackOverlay \
-    PrimaryClearOverlay \
-    PrimaryEtherealOverlay \
-    PrimaryFlameOverlay \
-    PrimaryNatureOverlay \
-    PrimaryOceanOverlay
+    SolarizedDarkSystemUIOverlay
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/primary
